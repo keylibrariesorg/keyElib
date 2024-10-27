76a14e7455552b6fc420d51145a2b19bd43ce213
forbidenid <muriithidennnis340@gmail.com>
2024-10-27 11:33:15 +0300

    commit update

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -127,13 +127,7 @@ const ProductCard = ({ data,isEvent }) => {
             color="#333"
             title="Quick view"
           />
-          <AiOutlineShoppingCart
-            size={25}
-            className="cursor-pointer absolute right-2 top-24"
-            onClick={() => addToCartHandler(data._id)}
-            color="#444"
-            title="Add to cart"
-          />
+          
           {open ? <ProductDetailsCard setOpen={setOpen} data={data} /> : null}
         </div>
       </div>
